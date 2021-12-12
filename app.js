eec63c21554481b4d801247a7f6aeeef0b63dfb4
KEITA <kabu.alice0818@gmail.com>
2021-12-12 19:14:40 +0900

    quiz

---
+++ app.js
@@ -0,0 +1,81 @@
+const quiz = [
+{
+  question: '最も販売数の多い漫画は、次の内どれ？',
+  answers: ['ドラゴンボール','ナルト','鬼滅の刃','one piece'],
+  correct: 'one piece'
+}, {
+
+  question: 'マイケルジョーダンの背番号は何番？',
+  answers: ['22','23','24','25'],
+  correct: '23'
+ }
+];
+
+const quizLength = quiz. length;
+let quizIndex = 0;
+let score = 0;
+
+const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
+
+//クイズの問題文、選択肢を定義
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
+  let buttonIndex = 0;
+  let buttonLength = $button.length;
+  while(buttonIndex < buttonLength){
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
+    buttonIndex++;
+  }
+}
+setupQuiz();
+
+const clickHandler = (e) => {
+  if(quiz[quizIndex].correct === e.target.textContent){
+    window.alert('正解!');
+    score++;
+  } else {
+    window.alert('不正解!');
+  } 
+
+  quizIndex++;
+
+  if(quizIndex < quizLength){
+    //問題数がまだあればこちらを実行
+    setupQuiz();
+  } else {
+    //問題数がもうなければこちらを実行
+    window.alert('終了!あなたの正解数は' + score + '/' + quizLength + 'です!');
+  }
+
+};
+ 
+// ボタンをクリックしたら正誤判定
+let handlerIndex = 0;
+while (handlerIndex < buttonLength) {
+  $button[handlerIndex].addEventListener('click', (e) => {
+      clickHandler(e);
+  });
+  handlerIndex++;
+}
+
+// $button[0].addEventListener('click', (e) => {
+//   clickHandler(e);
+// });
+
+// $button[1].addEventListener('click', (e) => {
+//    clickHandler(e);
+// });
+
+// $button[2].addEventListener('click', (e) => {
+//    clickHandler(e);
+// });
+
+// $button[3].addEventListener('click', (e) => {
+//    clickHandler(e);
+// });
+
+// $button[0].textContent = answers[0];
+// $button[1].textContent = answers[1];
+// $button[2].textContent = answers[2];
+// $button[3].textContent = answers[3];
